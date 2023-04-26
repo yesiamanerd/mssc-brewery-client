d08c4dfef4325f04fe78110e7e29f452a5677491
yesiamanerd <yesiamanerd@gmail.com>
2023-04-26 14:48:26 -0400

    Initial Commit of project

---
+++ src/test/java/com/yesiamanerd/msscbreweryclient/MsscBreweryClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yesiamanerd.msscbreweryclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBreweryClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
